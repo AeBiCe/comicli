2e283a212fa78f4b37d94801a9d997aa78505418
AeBiCe <info@aebice.net>
2025-03-28 22:20:41 +0100

    Setup devenv

---
+++ devenv.nix
@@ -0,0 +1,25 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/packages/
+  packages = [ pkgs.git ];
+
+  # https://devenv.sh/languages/
+  languages.python = {
+    enable = true;
+    venv.enable = true;
+    venv.requirements = ''
+      typer
+    '';
+  };
+
+  enterShell = ''
+    echo The Setup is complete 🤠
+  '';
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep --color=auto "${pkgs.git.version}"
+  '';
+}
